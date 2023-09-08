41456685ed182a54634c3d5338fb8c296e2d210c
RymMohamed <mhamedrim921@gmail.com>
2023-09-08 19:12:00 +0100

    eventService

--- src/app/public/services/event.service.ts
+++ src/app/public/services/event.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-  private eventsUrl = 'http://localhost:8081/api/arsii/event/allevents'; // Remplacez par l'URL de votre API
+  private eventsUrl = '/api/arsii/event/allevents'; // Remplacez par l'URL de votre API
 
   constructor(private http: HttpClient) { }
+  createRequestOptions() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5NDA3NzMwNCwiZXhwIjoxNjk0MDg3Mzg0fQ._b8q4bbl8awMeJd_QzhfT1B9u46F_2BHpyVh3f6m5yY',
+    });
+    return headers;
+  }
 
   getEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>(this.eventsUrl);
+    const headers = this.createRequestOptions();
+    return this.http.get<Event[]>(this.eventsUrl, {
+      headers: headers,
+    });
   }
+
 }
 
