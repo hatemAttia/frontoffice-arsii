65eea93b91f94832afbf7b1570276b434a5a5e94
RymMohamed <mhamedrim921@gmail.com>
2023-09-18 15:36:39 +0100

    breadcrumbs-component

--- src/app/public/components/events-details/events-details.component.ts
+++ src/app/public/components/events-details/events-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../../services/event.service';
 import { Event } from '../../types/event';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-events-details',
@@ -12,6 +13,10 @@ export class EventsDetailsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private eventService: EventService) { }
 
+  formatDate(dateString: string): string {
+    const eventDate = new Date(dateString);
+    return format(eventDate, 'dd LLLL yyyy, HH:mm');
+  }
   ngOnInit(): void {
     const idParam = this.route.snapshot.paramMap.get('id');
     if (idParam !== null) {
